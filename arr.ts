f1d151aadf5d2f41398205ee533155a4b0a28849
Alex <alexandre.ag.67@gmail.com>
2024-10-28 11:56:40 +0100

    ajout de la méthode pop() + les tests

--- arr.ts
+++ arr.ts
@@ -11,6 +11,31 @@ class Arr {
     this.value = array;
     return this.value.length;
   }
+
+  pop(): string | undefined {
+    // La méthode pop() supprime le dernier élément d'un tableau et retourne cet élément.
+    // Si le tableau est vide, la méthode pop() retourne undefined et le tableau n'est pas modifié.
+
+    // Si le tableau est vide, on retourne undefined
+    if (this.value.length === 0) {
+      return undefined;
+    }
+
+    // On récupère le dernier élément du tableau
+    const lastElement = this.value[this.value.length - 1];
+
+    // On crée un nouveau tableau de taille - 1
+    const array = new Array(this.value.length - 1);
+
+    // On copie les éléments du tableau actuel dans le nouveau tableau
+    for (let i = 0; i < array.length; i++) {
+      array[i] = this.value[i];
+    }
+
+    // On remplace le tableau actuel par le nouveau tableau
+    this.value = array;
+    return lastElement;
+  }
 }
 
 export default Arr;
--- test/array.test.ts
+++ test/array.test.ts
@@ -14,4 +14,26 @@ describe("Array", () => {
     expect(newArray.value).toHaveLength(1);
     expect(result).toEqual(1);
   });
+
+  it("should remove and return the last element of the array", () => {
+    const newArray = new Arr();
+    newArray.push("a");
+    newArray.push("b");
+    newArray.push("c");
+
+    const result = newArray.pop();
+
+    expect(result).toEqual("c");
+    expect(newArray.value).toHaveLength(2);
+    expect(newArray.value).toEqual(["a", "b"]);
+  });
+
+  it("should return undefined when pop is called on an empty array", () => {
+    const newArray = new Arr();
+
+    const result = newArray.pop();
+
+    expect(result).toBeUndefined();
+    expect(newArray.value).toHaveLength(0);
+  });
 });
