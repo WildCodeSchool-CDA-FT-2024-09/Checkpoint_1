bd385a30a0b7e2fbab1b4228e223d46ab031e2b0
Adri-bis <adrien.ma.16@hotmail.fr>
2024-10-28 12:12:15 +0100

    algo and test

--- arr.ts
+++ arr.ts
@@ -1,5 +1,6 @@
 class Arr {
   value: string[] = [];
+  length: number = 0;
 
   push(el: string): number {
     const array = new Array(this.value.length + 1);
@@ -11,6 +12,55 @@ class Arr {
     this.value = array;
     return this.value.length;
   }
+
+  apop() {
+    let longueur = 0;
+    for (let i: number = 0; i < this.value.length - 1; i++) {
+      {
+        longueur++;
+      }
+    }
+
+    const result = new Array(longueur);
+    for (let i: number = 0; i < this.value.length - 1; i++) {
+      result[i] = this.value[i];
+    }
+
+    return result;
+  }
+
+  ashift() {
+    let longueur = 0;
+    for (let i: number = 0; i < this.value.length - 1; i++) {
+      {
+        longueur++;
+      }
+    }
+    console.log(longueur);
+
+    const result = new Array(longueur);
+    console.log("avant boucle", result);
+
+    for (let i: number = 1; i < this.value.length; i++) {
+      result[i - 1] = this.value[i];
+      console.log(longueur);
+      console.log("boucle", result);
+    }
+
+    return result;
+  }
 }
 
+const arr = new Arr();
+arr.push("1");
+arr.push("2");
+arr.push("3");
+arr.push("4");
+
+console.log(arr);
+console.log(arr.length);
+const Aarr = arr.apop();
+console.log(Aarr);
+const Sarr = arr.ashift();
+console.log(Sarr);
 export default Arr;
--- test/array.test.ts
+++ test/array.test.ts
@@ -14,4 +14,30 @@ describe("Array", () => {
     expect(newArray.value).toHaveLength(1);
     expect(result).toEqual(1);
   });
+
+  it("Should remove the last element from the array", () => {
+    const newArray = new Arr();
+    newArray.push("1");
+    newArray.push("2");
+    newArray.push("3");
+
+    const result = newArray.apop();
+
+    expect(result).toEqual(["1", "2"]);
+    expect(result).toHaveLength(2);
+    expect(result).toBeInstanceOf(Array);
+  });
+
+  it("Should remove the first element from the array", () => {
+    const newArray = new Arr();
+    newArray.push("1");
+    newArray.push("2");
+    newArray.push("3");
+
+    const result = newArray.ashift();
+
+    expect(result).toEqual(["2", "3"]);
+    expect(result).toHaveLength(2);
+    expect(result).toBeInstanceOf(Array);
+  });
 });
