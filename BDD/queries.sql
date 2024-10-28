39d3aa8fd4734c6d87f1c8004a7ec01a17ceefb2
AkiraAcetonoshi <facilitateurmaker@gmail.com>
2024-10-28 11:39:13 +0100

    2: split the seeder into different files

---
+++ BDD/queries.sql
@@ -0,0 +1,7 @@
+
+    -- Toutes les offres d'emploi
+    select * from job
+    -- Toutes les offres d'emploi d'une ville (Paris)
+     select * from job where location='Paris'
+    -- Les informations de tous candidats qui ont postulé à une offre précise
+    -- Les informations de tous les candidats qui ont postulé aux offres d'une entreprise
--- BDD/seed.sql
+++
@@ -1,5 +0,0 @@
-insert into user_type (id, type) values (1,'recruiter');
-insert into user_type (id, type) values (2,'candidate');
-
-
-select * from user_type;
