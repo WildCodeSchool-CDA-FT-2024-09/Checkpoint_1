25571d988665927f53546a2b26e44527cdfa805f
Fabien Amann <fabien@feub.net>
2024-10-28 11:54:35 +0100

    Algo: popFab() + tests

--- arr.ts
+++ arr.ts
@@ -11,6 +11,27 @@ class Arr {
     this.value = array;
     return this.value.length;
   }
+
+  popFab() {
+    if (this.value.length === 0) return undefined;
+    const lastElement = this.value[this.value.length - 1];
+
+    this.value.length = this.value.length - 1;
+
+    return lastElement;
+  }
 }
 
 export default Arr;
+
+const myArr = new Arr();
+myArr.push("zen");
+myArr.push("noisette");
+myArr.push("suki");
+myArr.push("pedro");
+myArr.push("louloutte");
+console.log(myArr);
+
+const popRes = myArr.popFab();
+console.log(popRes);
+console.log(myArr);
--- test/array.test.ts
+++ test/array.test.ts
@@ -7,11 +7,30 @@ describe("Array", () => {
     expect(newArray.value).toHaveLength(0);
   });
 
-  it("should had an element in the array", () => {
+  it("should add an element in the array", () => {
     const newArray = new Arr();
     const result = newArray.push("a");
 
     expect(newArray.value).toHaveLength(1);
     expect(result).toEqual(1);
+  });
+
+  it("should removes the last element in the array and returns it", () => {
+    const newArray = new Arr();
+    newArray.push("zen");
+    newArray.push("pedro");
+    newArray.push("noisette");
+    newArray.push("suki");
+    newArray.push("louloutte");
+    const eltRemoved = newArray.popFab();
+
+    expect(eltRemoved).toEqual("louloutte");
+    expect(newArray.value).toHaveLength(4);
+
+    const newArray2 = new Arr();
+    const eltRemoved2 = newArray2.popFab();
+
+    expect(eltRemoved2).toEqual(undefined);
+    expect(newArray2.value).toHaveLength(0);
   });
 });
