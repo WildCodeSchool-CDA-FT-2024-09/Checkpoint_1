7f180f8eb771b6fdb21bbcb005e8003d2e49db81
pierre delarocque <delarocque.pierre@gmail.com>
2024-10-28 12:31:31 +0100

    ajout de test unitaire pour les methode pop et shift

--- test/array.test.ts
+++ test/array.test.ts
@@ -14,4 +14,28 @@ describe("Array", () => {
     expect(newArray.value).toHaveLength(1);
     expect(result).toEqual(1);
   });
+
+  it("should pop an element from the array", () => {
+    const newArray = new Arr();
+    newArray.push("a");
+    newArray.push("b");
+
+    const popped = newArray.pop();
+
+    expect(popped).toBe("b");
+    expect(newArray.value).toHaveLength(1);
+    expect(newArray.value[0]).toBe("a");
+  });
+
+  it("should shift an element from the array", () => {
+    const newArray = new Arr();
+    newArray.push("a");
+    newArray.push("b");
+
+    const shifted = newArray.shift();
+
+    expect(shifted).toBe("a");
+    expect(newArray.value).toHaveLength(1);
+    expect(newArray.value[0]).toBe("b");
+  });
 });
