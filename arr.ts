b4bcc872844620f23df7160cd5a0491d40f95fc3
scyanth <34488277+scyanth@users.noreply.github.com>
2024-10-28 12:09:12 +0100

    ajout methode pop avec test

--- arr.ts
+++ arr.ts
@@ -11,6 +11,16 @@ class Arr {
     this.value = array;
     return this.value.length;
   }
+
+  pop(): string {
+    const array = new Array(this.value.length - 1);
+
+    for (let i = 0; i < this.value.length - 1; i++) {
+      array[i] = this.value[i];
+    }
+    this.value = array;
+    return this.value[this.value.length - 1];
+  }
 }
 
 export default Arr;
--- test/array.test.ts
+++ test/array.test.ts
@@ -14,4 +14,14 @@ describe("Array", () => {
     expect(newArray.value).toHaveLength(1);
     expect(result).toEqual(1);
   });
+
+  it("should remove the last element of the array", () => {
+    const newArray = new Arr();
+    newArray.push("a");
+    newArray.push("b");
+    const result = newArray.pop();
+
+    expect(newArray.value).toHaveLength(1);
+    expect(result).toEqual("a");
+  });
 });
